2b3f4e12620df86d50b841acb8356da7ecf66e4f
Devin Walker <devin@wordimpress.com>
2016-11-29 16:27:32 -0800

    Fixed issue with deactivation logic not working as expected
    Moved plugin meta functions into activation file

--- includes/admin/give-paymill-activation.php
+++ includes/admin/give-paymill-activation.php
@@ -22,8 +22,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function give_paymill_activation_banner() {
 
-    // Check for if give plugin activate or not.
-    $is_give_active = defined( 'GIVE_PLUGIN_BASENAME' ) ? is_plugin_active( GIVE_PLUGIN_BASENAME ) : false ;
+	// Check for if give plugin activate or not.
+	$is_give_active = defined( 'GIVE_PLUGIN_BASENAME' ) ? is_plugin_active( GIVE_PLUGIN_BASENAME ) : false;
 
 	//Check to see if Give is activated, if it isn't deactivate and show a banner
 	if ( is_admin() && current_user_can( 'activate_plugins' ) && ! $is_give_active ) {
@@ -31,7 +31,24 @@ function give_paymill_activation_banner() {
 		add_action( 'admin_notices', 'give_paymill_activation_notice' );
 
 		//Don't let this plugin activate
-		deactivate_plugins( plugin_basename( __FILE__ ) );
+		deactivate_plugins( GIVE_PAYMILL_BASENAME );
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		return false;
+
+	}
+
+
+	//Check minimum Give version.
+	if ( defined( 'GIVE_VERSION' ) && version_compare( GIVE_VERSION, GIVE_PAYMILL_MIN_GIVE_VERSION, '<' ) ) {
+
+		add_action( 'admin_notices', 'give_paymill_min_version_notice' );
+
+		//Don't let this plugin activate.
+		deactivate_plugins( GIVE_PAYMILL_MIN_GIVE_VERSION );
 
 		if ( isset( $_GET['activate'] ) ) {
 			unset( $_GET['activate'] );
@@ -42,23 +59,23 @@ function give_paymill_activation_banner() {
 	}
 
 	//Check for activation banner inclusion
-	$activation_banner_file = GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php';
-	if ( ! class_exists( 'Give_Addon_Activation_Banner' ) && file_exists( $activation_banner_file ) ) {
-		include $activation_banner_file;
+	if ( ! class_exists( 'Give_Addon_Activation_Banner' ) && file_exists( GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php' ) ) {
+
+		include GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php';;
+
+		//Only runs on admin
+		$args = array(
+			'file'              => __FILE__,
+			'name'              => esc_html__( 'Paymill Gateway', 'give-paymill' ),
+			'version'           => GIVE_PAYMILL_VERSION,
+			'settings_url'      => admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways' ),
+			'documentation_url' => 'https://givewp.com/documentation/add-ons/paymill-gateway/',
+			'support_url'       => 'https://givewp.com/support/',
+			'testing'           => false
+		);
+
+		new Give_Addon_Activation_Banner( $args );
 	}
-
-	//Only runs on admin
-	$args = array(
-		'file'              => __FILE__,
-		'name'              => esc_html__( 'Paymill Gateway', 'give-paymill' ),
-		'version'           => GIVE_PAYMILL_VERSION,
-		'settings_url'      => admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways' ),
-		'documentation_url' => 'https://givewp.com/documentation/add-ons/paymill-gateway/',
-		'support_url'       => 'https://givewp.com/support/',
-		'testing'           => false
-	);
-
-	new Give_Addon_Activation_Banner( $args );
 
 	return false;
 
@@ -67,10 +84,87 @@ function give_paymill_activation_banner() {
 add_action( 'admin_init', 'give_paymill_activation_banner' );
 
 /**
- * Notice for No Core Activation
+ * Notice for No Core Activation.
  *
  * @since 1.1
  */
 function give_paymill_activation_notice() {
-	echo '<div class="error"><p>' . __( '<strong>Activation Error:</strong> We noticed Give is not active. Please activate Give in order to use the Paymill Gateway.', 'give-paymill' ) . '</p></div>';
+	echo '<div class="error"><p>' . __( '<strong>Activation Error:</strong> You must have the <a href="https://givewp.com/" target="_blank">Give</a> plugin installed and activated for the Paymill add-on to activate.', 'give-paymill' ) . '</p></div>';
 }
+
+/**
+ * Notice for no core activation.
+ *
+ * @since 1.1
+ */
+function give_paymill_min_version_notice() {
+	echo '<div class="error"><p>' . sprintf( __( '<strong>Activation Error:</strong> You must have <a href="%s" target="_blank">Give</a> version %s+ for the Paymill add-on to activate.', 'give-paymill' ), 'https://givewp.com', GIVE_PAYMILL_MIN_GIVE_VERSION ) . '</p></div>';
+}
+
+
+/**
+ * Plugins row action links
+ *
+ * @since 1.1
+ *
+ * @param array $actions An array of plugin action links.
+ *
+ * @return array An array of updated action links.
+ */
+function give_paymill_plugin_action_links( $actions ) {
+	$new_actions = array(
+		'settings' => sprintf(
+			'<a href="%1$s">%2$s</a>',
+			admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways' ),
+			esc_html__( 'Settings', 'give-paymill' )
+		),
+	);
+
+	return array_merge( $new_actions, $actions );
+}
+
+add_filter( 'plugin_action_links_' . GIVE_PAYMILL_BASENAME, 'give_paymill_plugin_action_links' );
+
+
+/**
+ * Plugin row meta links
+ *
+ * @since 1.1
+ *
+ * @param array  $plugin_meta An array of the plugin's metadata.
+ * @param string $plugin_file Path to the plugin file, relative to the plugins directory.
+ *
+ * @return array
+ */
+function give_paymill_plugin_row_meta( $plugin_meta, $plugin_file ) {
+	if ( $plugin_file != GIVE_PAYMILL_BASENAME ) {
+		return $plugin_meta;
+	}
+
+	$new_meta_links = array(
+		sprintf(
+			'<a href="%1$s" target="_blank">%2$s</a>',
+			esc_url( add_query_arg( array(
+					'utm_source'   => 'plugins-page',
+					'utm_medium'   => 'plugin-row',
+					'utm_campaign' => 'admin',
+				), 'https://givewp.com/documentation/add-ons/paymill-gateway/' )
+			),
+			esc_html__( 'Documentation', 'give-paymill' )
+		),
+		sprintf(
+			'<a href="%1$s" target="_blank">%2$s</a>',
+			esc_url( add_query_arg( array(
+					'utm_source'   => 'plugins-page',
+					'utm_medium'   => 'plugin-row',
+					'utm_campaign' => 'admin',
+				), 'https://givewp.com/addons/' )
+			),
+			esc_html__( 'Add-ons', 'give-paymill' )
+		),
+	);
+
+	return array_merge( $plugin_meta, $new_meta_links );
+}
+
+add_filter( 'plugin_row_meta', 'give_paymill_plugin_row_meta', 10, 2 );
